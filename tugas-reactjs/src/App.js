6ee022e4fdbc834516a095d7c039051ffae5163c
Diana Kusuma <diaanakusuma@gmail.com>
2023-03-13 21:49:43 +0700

    push tugas6

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="Container">
+          <div className="Title">Hi, saya Diana</div>
+          <br />
+          <hr />
+          <p>Saya dari Batch 43 sudah berhasil menginstal React</p>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
