3f2339696faf64b5b39c5358b5b5f0eaeb47ad7d
Diana Kusuma <diaanakusuma@gmail.com>
2023-03-25 00:01:19 +0700

    puah tugas 12

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -6,6 +6,10 @@ import Tugas9 from './tugas9/tugas9';
 import Tugas10 from './tugas10/tugas10';
 import Tugas11 from './tugas11/tugas11';
 
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Navbar from './components/navbar';
+
+
 const App = () => {
   return(
     <>
@@ -13,8 +17,21 @@ const App = () => {
       <Tugas7 name="Diana Kusuma" batch="43" email="diaanakd@gmail.com"/>
       <Tugas8/>
       <Tugas9/>
-      <Tugas10/> */}
-      <Tugas11/>
+      <Tugas10/>
+      <Tugas11/> */}
+
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={ <Tugas6 /> }/>
+          <Route path='/tugas7' element={ <Tugas7 name="Diana Kusuma" batch="43" email="diaanakd@gmail.com" /> } />
+          <Route path='/tugas8' element={ <Tugas8 /> }/>
+          <Route path='/tugas9' element={ <Tugas9 /> }/>
+          <Route path='/tugas10' element={ <Tugas10 /> }/>
+          <Route path='/tugas11' element={ <Tugas11 /> }/>
+        </Routes>
+      </BrowserRouter>
+
     </>
   );
 }
